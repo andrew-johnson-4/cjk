41b67985ac5987185f05ab99005ab40f01577590
Andrew Johnson <andrewjohnson@Andrews-MacBook-Pro.local>
2020-08-14 13:36:59 -0700

    add test confirming non-null variants

--- tests/variants.rs
+++ tests/variants.rs
@@ -10,3 +10,8 @@ fn variants2() {
    assert_eq!(cjk::variants('儒'), vec!['㐵']);
    assert_eq!(cjk::variants('㐵'), vec!['儒']);
 }
+
+#[test]
+fn variants3() {
+   assert_eq!(cjk::variants(' '), vec![]);
+}
